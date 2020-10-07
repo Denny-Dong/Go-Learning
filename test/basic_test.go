e6b6a8a037c0f6c062a3ad0509135c2799c1d724
Denny-Dong <coolbird1983@163.com>
2020-10-07 10:50:09 +0800

    update project

--- test/basic_test.go
+++ test/basic_test.go
@@ -5,3 +5,16 @@ import "testing"
 func TestVariable(t *testing.T) {
 
 }
+
+func TestFibonacci(t *testing.T) {
+	a := 1
+	b := 1
+	t.Log(a)
+	t.Log(b)
+	for i := 0; i < 10; i++ {
+		c := a + b
+		t.Log(c)
+		a = b
+		b = c
+	}
+}
