ea0e322ffa05b40077308b905359d66c3a49353f
Denny Dong <coolbird1983@163.com>
2020-10-08 21:01:38 +0800

    update project

--- test/for_test.go
+++ test/for_test.go
@@ -25,5 +25,30 @@ func TestSwitch(t *testing.T) {
 	default:
 		t.Log("default")
 	}
+}
 
+func fibonacciNormal(n int) int {
+	a := 1
+	b := 1
+	for i := 2; i < n; i++ {
+		c := b
+		b = a + b
+		a = c
+	}
+	return b
+}
+
+func fibonacciRecursion(n int) (result int) {
+	if n <= 2 {
+		result = 1
+		return result
+	} else {
+		result = fibonacciRecursion(n-1) + fibonacciRecursion(n-2)
+		return result
+	}
+}
+
+func TestFibonacci1(t *testing.T) {
+	t.Log(fibonacciNormal(10))
+	t.Log(fibonacciRecursion(10))
 }
