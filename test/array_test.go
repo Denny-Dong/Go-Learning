ffbb9cf6c66e884349d6eb09b03ce43c9102d2e8
Denny Dong <coolbird1983@163.com>
2020-10-08 21:47:36 +0800

    update project

--- test/array_test.go
+++ test/array_test.go
@@ -1,8 +1,16 @@
 package test
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestArray(t *testing.T) {
 	var student = [5]string{"a", "b", "c", "d", "e"}
+	var teacher = [...]string{"a", "b", "c"}
 	t.Log(student)
+	t.Log(teacher)
+	for index, value := range teacher {
+		t.Log(index)
+		t.Log(value)
+	}
 }
